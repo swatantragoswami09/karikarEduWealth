aec2a9c39049503f7fdc5b6757e2e78f1b9ddd88
NAGARRO\swatantragoswami <swatantra.goswami_cs17@nagarro.com>
2021-12-30 20:02:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from "./logo.svg";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import login from "./pages/login/Login";
+import signup from "./pages/signup/Signup";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Karikar EduWealth Project</h1>
+      <BrowserRouter>
+        <h1>Karikar EduWealth Project</h1>
+        <Switch>
+          <Route path="/login" exact component={login} />
+          <Route path="/signup" exact component={signup} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
